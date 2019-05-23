2e4df2d951aa32055a18a16d57872c832db14205
Peter Mackay <project2501@gmail.com>
2019-05-23 15:51:21 -0600

    all board crud actions and views implemented

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,53 @@
 class BoardsController < ApplicationController
+ before_action :set_board, only:[:show, :edit, :update, :destroy]
+ 
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      redirect_to :new
+    end
   end
 
   def edit
+    render partial: "form"
   end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path
+  end
+
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+
 end
